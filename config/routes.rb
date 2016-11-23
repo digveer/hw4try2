b50931603bbe8f5c89e94d40eb0bfe05b55114cb
digveer <digveer@outlook.com>
2016-11-22 20:16:56 -0600

    add all

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rottenpotatoes::Application.routes.draw do
+  #resources :movies
+  # map '/' to be a redirect to '/movies'
+  #root :to => redirect('/movies')
+  #get '/' =>"movies#index"
+  
+  resources :movies
+  root :to => redirect('/movies')
+	match '/movies/:id/similar', to: 'movies#similar', as: 'similar_movie', via: :get
+end
